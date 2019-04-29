706b875331e5a66305df8be06c7870a41b00d1fd
Ash <ashley.cruz.crespo@gmail.com>
2019-04-29 15:05:47 -0400

    fixing some bugs

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+export default function ActionButton(props) {
+    return (
+        (props.large)
+            ? <div className="btn action large" onClick={props.updateDisplay}>
+                    {props.content}
+              </div> 
+            : <div className="btn action" onClick={props.updateDisplay}>
+                    {props.content}
+              </div>
+    )
+}
